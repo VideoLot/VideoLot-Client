4186d1bdf853b0f4be0cefe190e3e967e7f0844b
Alexey Sukhov <a.sukhov93@gmail.com>
2023-10-01 16:44:09 +0300

    seek buttons removed

--- src/app/components/player/components/playback-buttons.tsx
+++ src/app/components/player/components/playback-buttons.tsx
@@ -24,10 +24,8 @@ export function PlaybackButtons() {
         'bg-play-button-texture';
     return (
         <div className='flex h-1/4 pointer-events-auto space-x-2'>
-            <button className='bg-rewind-button-texture bg-no-repeat bg-cover h-4/5 self-center aspect-square' />
             <button
                 onClick={playButtonClick}
                 className={`${playButtonBg} bg-no-repeat bg-cover aspect-square`} />
-            <button className='bg-fast-forward-button-texture bg-no-repeat bg-cover h-4/5 self-center aspect-square' />
         </div>);
 }
