351eb9eaa6c37c3b9802394253914d33116e7fbf
Alexey Sukhov <a.sukhov93@gmail.com>
2023-10-09 00:46:59 +0300

    Keep panels order

--- src/app/components/panels/server-root-panel.tsx
+++ src/app/components/panels/server-root-panel.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/utils/db'
-import { Panel } from '@videolot/videolot-prisma'
+import { Panel, Prisma } from '@videolot/videolot-prisma'
 import { PanelContentData } from '../../types';
 import ServerPanel from './server-panel';
 
@@ -15,9 +15,20 @@ export default async function ServerRootPanel( {panel}: RootPanelProps) {
     }
     
     const panelIds = data.filter as string[];
-    const panels = await prisma.panel.findMany({
-        where: {id: {in: panelIds}}
-    });
+    // const panels = await prisma.panel.findMany({
+    //     where: {id: {in: panelIds}}
+    // });
+    // .map(x=>`'${x}'`)
+    
+    // const queryIds = panelIds.map(x=> `'${x}'`).join(', ')//
+    const queryIds = Prisma.join(panelIds);
+    const idsOrder = `{${panelIds.map(x=>`${x}`).join(', ')}}`
+    const panels = await prisma.$queryRaw(Prisma.sql`
+        SELECT * FROM public."Panel" 
+        JOIN unnest(${idsOrder}::text[]) WITH ORDINALITY t(id, ord) using (id)
+        WHERE id IN (${queryIds})
+        ORDER BY t.ord
+        ;`) as Panel[];
 
     return <>
     {
